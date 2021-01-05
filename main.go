8a78b2749419f7d16618d879181b057776aba900
tsny <tsny700@gmail.com>
2021-01-04 23:13:29 -0600

    updates

--- main.go
+++ main.go
@@ -10,74 +10,38 @@ import (
 )
 
 const (
-	url    = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=BTC-USD"
-	up     = "🟩"
-	down   = "🟥"
-	format = "15:04:05"
+	url     = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=BTC-USD"
+	up      = "🟩"
+	down    = "🟥"
+	neutral = "🟦"
+	alert   = "☎️"
+	format  = "15:04:05"
 )
 
 var lastPrice = 0.00
 var price = 0.00
-var priceAtLastHourMark = 0.00
-var priceAtLast5MinMark = 0.00
-var timeOfLastHourMark time.Time
-var timeOfLast5MinMark time.Time
-var minutesPassed = 0
-var streak Streak
 
 func main() {
 	banner("Fetching BTC Prices...")
 	for {
 		price = fetchData()
-		if minutesPassed == 0 {
-			priceAtLastHourMark = price
-			priceAtLast5MinMark = price
-			timeOfLastHourMark = time.Now()
-			timeOfLast5MinMark = time.Now()
-		}
-		diff := price - lastPrice
-		emoji := getEmoji()
-		t := time.Now().Format(format)
-
-		if lastPrice == 0.00 {
-			fmt.Printf("%s %s: $%.2f\n", emoji, t, price)
-		} else {
-			fmt.Printf("%s %s: $%v | Change: $%.2f\n", emoji, t, price, diff)
-		}
-
+		onDataUpdated()
 		lastPrice = price
 		time.Sleep(60 * time.Second)
-
-		minutesPassed++
-		if minutesPassed > 59 {
-			onHourPassed()
-		} else if minutesPassed%5 == 0 {
-			onFiveMinPassed()
-		}
 	}
 }
 
-func banner(str string) {
+func banner(str string, args ...interface{}) {
+	str = sf(str, args...)
 	b := strings.Repeat("-", len(str))
 	fmt.Printf("%s\n%s\n%s\n", b, str, b)
 }
 
-func onFiveMinPassed() {
-	timeOfLast5MinMark = time.Now()
-	banner(fmt.Sprintf("5 Minutes Passed - Change: $%.2f", price-priceAtLast5MinMark))
-	priceAtLast5MinMark = price
-}
-
-func onHourPassed() {
-	timeOfLastHourMark = time.Now()
-	minutesPassed = 0
-	banner(fmt.Sprintf("Hour Passed - Change: $%.2f", price-priceAtLastHourMark))
-	priceAtLastHourMark = price
-}
-
-func getEmoji() string {
-	if price > lastPrice {
+func getEmoji(curr, prev float64) string {
+	if prev < curr {
 		return up
+	} else if prev == curr {
+		return neutral
 	}
 	return down
 }
--- streak.go
+++ streak.go
@@ -1,7 +1,76 @@
 package main
 
-type Streak struct {
-	positive bool
+import (
+	"fmt"
+	"time"
+)
+
+type streak struct {
+	positive    bool
 	occurrences int
-	total float64
+	total       float64
+}
+
+type interval struct {
+	beginPrice       float64
+	occurrences      int
+	maxOccurences    int
+	percentThreshold float64
+	startTime        time.Time
+}
+
+var intervals []*interval = []*interval{
+	&interval{0.0, 0, 60, 2, time.Now()},
+	&interval{0.0, 0, 30, 1, time.Now()},
+	&interval{0.0, 0, 5, .5, time.Now()},
+}
+
+var sf = fmt.Sprintf
+
+func onDataUpdated() {
+	intervalCompleted := false
+	for _, i := range intervals {
+		if i.beginPrice == 0 {
+			i.beginPrice = price
+		}
+		i.occurrences++
+		if i.occurrences >= i.maxOccurences {
+			i.onCompleted()
+			i.reset()
+			intervalCompleted = true
+		}
+	}
+	if !intervalCompleted {
+		t := getTime()
+		emoji := getEmoji(price, lastPrice)
+		diff := price - lastPrice
+		percent := (diff / price) * 100
+		if lastPrice == 0.00 {
+			fmt.Printf("%s %s: $%.2f \n", emoji, t, price)
+		} else {
+			fmt.Printf("%s %s: $%.2f | Change: $%.2f | Percent: %.3f%% \n", emoji, t, price, diff, percent)
+		}
+	}
+}
+
+func (i *interval) onCompleted() {
+	diff := price - i.beginPrice
+	percent := (diff / i.beginPrice) * 100
+	prefix := ""
+	if percent > i.percentThreshold {
+		prefix = sf("%s ALERT: Threshold of %f%% was reached! ", alert, i.percentThreshold)
+	}
+	totalChange := sf("%.2f --> %.2f", i.beginPrice, price)
+	changes := sf("Change: $%.2f | Percent: %.3f%%", diff, percent)
+	banner("%s: %s%d Minutes Passed | %s | %s", getTime(), prefix, i.occurrences, totalChange, changes)
+}
+
+func getTime() string {
+	return time.Now().Format(format)
+}
+
+func (i *interval) reset() {
+	i.occurrences = 0
+	i.startTime = time.Now()
+	i.beginPrice = price
 }
